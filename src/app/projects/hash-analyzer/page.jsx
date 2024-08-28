8c612ce0010ca434c550af0511630f9ff6737979
Abdul Mannan Nadkar <abdulmannannadkar@RADEON-105.local>
2024-08-28 08:47:19 +0530

    first commit

---
+++ src/app/projects/hash-analyzer/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <form action=""> Input Hashes: <input type="text" placeholder='Hashes' /> 
+    <div>API key: <input type="text" placeholder='Input your API key'/></div>
+    </form>
+    </>
+  )
+}
+
+export default page
